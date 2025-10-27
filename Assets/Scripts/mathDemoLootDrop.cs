561f945b148bdb4a25e9de910637860d1d2d2a6b
CF8339816 <chris_french_3dga@hotmail.com>
2025-10-27 11:51:18 -0300

    added canvas text field for outpit  set up script

---
+++ Assets/Scripts/mathDemoLootDrop.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class mathDemo : MonoBehaviour
+{
+    [Header("Chest Type")]
+    [SerializeField] int Wood_Chest = 0;
+    [SerializeField] int Bronze_Chest = 0;
+    [SerializeField] int Silver_Chest = 0;
+    [SerializeField] int Gold_Chest = 0;
+    [SerializeField] int Platnum_Chest = 0;
+
+
+    [Header("Wooden Chest")]
+    [SerializeField] int Wood_Chest_Common = 0;
+    [SerializeField] int Wood_Chest_Uncommon = 0;
+    [SerializeField] int Wood_Chest_Rare = 0;
+    [SerializeField] int Wood_Chest_Epic = 0;
+    [SerializeField] int Wood_Chest_Legendary = 0;
+
+
+    [Header("Bronze Chest")]
+    [SerializeField] int Bronze_Chest_Common = 0;
+    [SerializeField] int Bronze_Chest_Uncommon = 0;
+    [SerializeField] int Bronze_Chest_Rare = 0;
+    [SerializeField] int Bronze_Chest_Epic = 0;
+    [SerializeField] int Bronze_Chest_Legendary = 0;
+
+
+    [Header("Silver Chest")]
+    [SerializeField] int Silver_Chest_Common = 0;
+    [SerializeField] int Silver_Chest_Uncommon = 0;
+    [SerializeField] int Silver_Chest_Rare = 0;
+    [SerializeField] int Silver_Chest_Epic = 0;
+    [SerializeField] int Silver_Chest_Legendary = 0;
+
+    [Header("Gold Chest")]
+    [SerializeField] int Gold_Chest_Common = 0;
+    [SerializeField] int Gold_Chest_Uncommon = 0;
+    [SerializeField] int Gold_Chest_Rare = 0;
+    [SerializeField] int Gold_Chest_Epic = 0;
+    [SerializeField] int Gold_Chest_Legendary = 0;
+
+    [Header("Platinum Chest")]
+    [SerializeField] int Platinum_Chest_Common = 0;
+    [SerializeField] int Platinum_Chest_Uncommon = 0;
+    [SerializeField] int Platinum_Chest_Rare = 0;
+    [SerializeField] int Platinum_Chest_Epic = 0;
+    [SerializeField] int Platinum_Chest_Legendary = 0;
+    //[SerializeField] int Platinum_Chest_Drops = Platinum.Count{0};
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
